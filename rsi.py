6ec35d796ed3a4d0d9fc5450894d48ef470bef12
sjalford76 <s_j_alford@hotmail.com>
2024-02-25 14:56:15 +0000

    rsi tidy up.

--- rsi.py
+++ rsi.py
@@ -1,9 +1,9 @@
 import numpy
 from helpers import nanarray
+import average
 
 def rsi( data, n):
-        diffs = numpy.empty( len(data) )
-        diffs.fill(numpy.nan)
+        diffs = nanarray(len(data))
         diffs[1:] = numpy.diff(data)
 
         ups = numpy.array([ u if u >= 0 else 0 for u in diffs ])
@@ -12,12 +12,8 @@ def rsi( data, n):
         up_average = nanarray(len(data))
         down_average = nanarray(len(data))
 
-        up_average[n] = sum(ups[:n+1])/n
-        down_average[n] = sum(downs[:n+1])/n
-
-        for i in range(n+1, len(data)):
-                up_average[i] = ((n - 1)*up_average[i-1] + ups[i] )/ n
-                down_average[i] = ((n - 1)*down_average[i-1] + downs[i] )/n
+        up_average = average.wilder_average(ups, n)
+        down_average = average.wilder_average(downs, n)
 
         #Step 7 5/6
         average_up_down = nanarray(len(data))
