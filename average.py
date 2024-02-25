080e0e2ba93bafcd5cef457bcbe15d3c7bc538cd
sjalford76 <s_j_alford@hotmail.com>
2024-02-25 14:33:14 +0000

    wilder average

--- average.py
+++ average.py
@@ -3,15 +3,15 @@ import helpers
 
 def rolling_average(srs, n):
     c = numpy.cumsum(srs, dtype=float)
-    res = helpers.nanarray(n)
+    res = helpers.nanarray(len(srs))
     res[n:] = (c[n:]- c[:-n]) / n
     return res
 
-def wilder_average(srs, n):
-    result = helpers.nanarray(n)
-    result[n-1] = sum(srs[:n])/n
+def wilder_average(tr, n):
+    result = helpers.nanarray(len(tr))
+    result[n-1] = sum(tr[:n])/n
     
-    for i in range(n, len(srs)):
-        result[i] = ((n-1)*result[i-1] + srs)/n
+    for i in range(n, len(tr)):
+        result[i] = ((n-1)*result[i-1] + tr[i])/n
 
     return result
